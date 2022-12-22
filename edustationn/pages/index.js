1e585e86a26b5a879f04b043604e5275e134102a
Lokesh <iamlokeshahire@gmail.com>
2022-12-22 20:47:53 +0530

    assognment

--- edustationn/pages/index.js
+++ edustationn/pages/index.js
@@ -1,11 +1,13 @@
-import Login from "./login";
-
+// import Assignment from "./Assignment/[id]";
+import AssignmentPage from "./AssignmentPage";
+// import Login from "./login";
 
 export default function Home() {
   return (
     <>
       {/* <Login/> */}
-      hello
+      <AssignmentPage />
+      {/* hello */}
     </>
-  )
+  );
 }
--- edustationn/pages/login.js
+++ edustationn/pages/login.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+// import axios from 'axios'
 
 const Login = (meta) => {
-console.log(meta.name)
-  return (
-    <div>hello</div>
-  )
+  console.log(meta.name);
+  return <div>hello</div>;
+};
+
+export default Login;
+
+export async function getStaticProps(context) {
+  // const query = context.query;
+  // const {id} = query
+
+  const info = await axios.get(`http://localhost:3000/api/login`);
+  const meta = info.data;
+  console.log(meta.name);
+
+  return {
+    props: { data: { meta } },
+  };
 }
-
-export default Login
-
-
-export async function getStaticProps(context){
-
-    // const query = context.query;
-    // const {id} = query
-
-    const info = await axios.get(`http://localhost:3000/api/login`)
-    const meta = info.data
-    console.log(meta.name)
-
-    return {
-        props:{ data: {meta}}
-    }
-} 
-
